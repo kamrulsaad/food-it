832bef86210458b3a4f9490630df85d00e812e5d
kamrulsaad <skamrul2680@gmail.com>
2025-04-29 15:46:29 +0600

    Added Font Subseta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Toaster } from "@/components/ui/sonner";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
